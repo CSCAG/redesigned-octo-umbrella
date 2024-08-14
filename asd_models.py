33c460496dee7fa52b59eba5a9f6a1547c986862
Hashim Ali <hashim_ali94@outlook.com>
2024-08-14 13:21:47 -0400

    added Rawnet model and corrected the scoring error. Now scores are consistent among multiple runs

--- asd_models.py
+++ asd_models.py
@@ -7,36 +7,89 @@ import torch
 from importlib import import_module
 from typing import Dict, List, Union
 from torch import Tensor
+import soundfile as sf
+import librosa
+import yaml
+
+from models.model import RawNet
 
 
 class ASD():
     
-    def __init__(self, model_type='aasist', config_path='./config/AASIST_ASVspoof5.conf', generate_score_file=False):
+    def __init__(self, model_type='aasist', generate_score_file=False, save_path = './score_files/'):
 
         self.model_type = model_type
         self.gen_score_file = generate_score_file
 
+        #GPU device
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'                  
+        print('Device: {}'.format(self.device))
+
         if self.gen_score_file:
 
-            self.save_path = save_path = './score_files/'
+            self.save_path = save_path
 
             if not os.path.exists(self.save_path):
                 os.makedirs(self.save_path)
 
         if self.model_type == 'aasist':
 
-            with open('./config/AASIST_ASVspoof5.conf', "r") as f_json:
+            config_path='./config/AASIST_ASVspoof5.conf'
+
+            with open(config_path, "r") as f_json:
                 config = json.loads(f_json.read())
 
             self.config = config
             self.model_config = config["model_config"]
 
-            self.model = get_model(self.model_config)
+            self.model = get_model(self.model_config, self.device)
+
+            self.model.load_state_dict(torch.load(config["model_path"], map_location=self.device))
+
+        if self.model_type == 'rawnet':
+
+            dir_yaml = os.path.splitext('./config/model_config_RawNet')[0] + '.yaml'
+
+            with open(dir_yaml, 'r') as f_yaml:
+                parser1 = yaml.load(f_yaml, yaml.Loader)
+
+                model = RawNet(parser1['model'], self.device)
+                nb_params = sum([param.view(-1).size()[0] for param in model.parameters()])
+                self.model =(model).to(self.device)
+                print("no. model params:{}".format(nb_params))
+
+            model_path = './models/weights/RawNet2/RawNet2_best_model_laundered_train.pth'
+
+            if model_path:
+                model.load_state_dict(torch.load(model_path,map_location=self.device))
+                print('Model loaded : {}'.format(model_path))
 
     
-    def run(self, audio_data: np.ndarray):
+    def run(self, audio_data_dict: Dict, use_saved_chunks = False):
 
-        score_df = self.produce_evaluation(data_dict=audio_data)
+        if use_saved_chunks:
+
+            chunk_dir = './chunks'
+            # chunk_dir = '/data/Famous_Figures/AES_Data/aes_data'
+
+            chunk_files = os.listdir(chunk_dir)
+            
+            audio_data_dict = {}
+            for cf in chunk_files:
+                
+                cf_filename = os.path.join(chunk_dir, cf)
+                
+                # audio_data, sr = sf.read(cf_filename)
+                audio_data, fs = librosa.load(cf_filename, sr=16000)
+
+                # audio_data = audio_data[:,0]
+
+                audio_data_dict[cf.split('.')[0]] = audio_data
+
+            score_df = self.produce_evaluation(data_dict=audio_data_dict)
+
+        else:
+            score_df = self.produce_evaluation(data_dict=audio_data_dict)
 
         return score_df
 
@@ -58,11 +111,16 @@ class ASD():
             X_pad = pad(audio_data, 64600)
             x_inp = Tensor(X_pad)
 
-            x_inp = x_inp.unsqueeze(0).float()
+            x_inp = x_inp.unsqueeze(0).float().to(self.device)
 
             with torch.no_grad():
-                _, audio_out = model(x_inp)
+                if self.model_type == 'aasist':
+                    _, audio_out = model(x_inp)
+                else:
+                    audio_out = model(x_inp)
+
                 audio_score = (audio_out[:, 1]).data.cpu().numpy().ravel()
+                # print(audio_score)
             
             # add outputs
             fname_list.append(utt_id)
@@ -74,20 +132,20 @@ class ASD():
         score_df = pd.DataFrame(data=score_dict)
 
         if self.gen_score_file:
-            score_df.to_csv(self.save_path, index=False, sep="\t")
+            score_df.to_csv(os.path.join(self.save_path, self.model_type + '_out_scores.txt'), index=False, sep="\t")
 
             print("Scores saved to {}".format(self.save_path))
 
         return score_df
 
 
-def get_model(model_config: Dict):
+def get_model(model_config: Dict, device):
     """Define DNN model architecture"""
     module = import_module("models.{}".format(model_config["architecture"]))
     _model = getattr(module, "Model")
     # model = nn.DataParallel(_model(model_config))
     model = _model(model_config)
-    # model = model.to(device)
+    model = model.to(device)
     nb_params = sum([param.view(-1).size()[0] for param in model.parameters()])
     print("no. model params:{}".format(nb_params))
 
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def upload(url, data):
     # we can either save the audio file locally and then pass the audio filename and save path to the preprocessing block
     # OR, we can read the audio file directly and pass the audio data to the preprocesing block
     data_dir = './data'
-    filename = 'A Promised Land - 004.wav'
+    filename = 'Barack_Obama.wav'
 
     audio_file = os.path.join(data_dir, filename)
 
@@ -26,9 +26,10 @@ def upload(url, data):
 
     ############# Preprocessing block starts here #####################################
 
-    audio_chunks_dict = preprocess_audio(audio_file, save_chunks=False)
+    # audio_chunks_dict = preprocess_audio(audio_file, save_chunks=True)
+    audio_chunks_dict = {}
 
-    print(audio_chunks_dict)
+    # print(audio_chunks_dict)
 
     # format for audio_chunks_dict = {chunk_1: audio_data, chunk_2: audio_data, ...}
 
@@ -39,9 +40,11 @@ def upload(url, data):
 
     ############### ASD Algorithm #####################################################
 
-    asd_model = ASD()
+    asd_model = ASD(model_type='aasist', generate_score_file=True)
 
-    asd_model.run(audio_chunks_dict)
+    score_df = asd_model.run(audio_chunks_dict, use_saved_chunks=True)
+
+    print(score_df)
 
 
 ############### main ################
